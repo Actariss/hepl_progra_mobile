f8cbdeb21dc0fac66a4c43e1d88c0fa9a5073dd7
Actariss <l.naa@student.helmo.be>
2025-01-16 08:17:50 +0100

    Test de la version sauvegarde d'état en DB

--- lib/Exercices/mymain.dart
+++ lib/Exercices/mymain.dart
@@ -25,11 +25,17 @@ class ToDoList extends StatefulWidget {
 
 class _ToDoListState extends State<ToDoList> {
   final taskController = TextEditingController();
+  late Future<List<Map<String, Object?>>>taskListInit;
 
   bool checked = false;
   bool mabool = false;
   final List<Map<String, dynamic>> checkIt = [];
+
   @override
+  void initState(){
+    super.initState();
+    taskListInit = TaskController.getTasks();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,50 +80,65 @@ class _ToDoListState extends State<ToDoList> {
               child: const Text("Add Task")),
           ElevatedButton(
               onPressed: (){setState(() {
+                TaskController.deleteTasks();
                 checkIt.clear();
               });},
               child: const Text("Clear Data")),
           ]),
           Expanded(
-              child: ListView.builder(
-                  itemCount: checkIt.length,
-                  itemBuilder: (context, index) {
-                    return Row(
-                      children: [
-                    FutureBuilder<bool?>(
-                    future: TaskController.getTaskState(checkIt[index]['text']), // Obtenez l'état
-                    builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                    // Affichez un indicateur de chargement en attendant les données
-                    return const CircularProgressIndicator();
+              child: FutureBuilder(
+                future: taskListInit,
+                builder: (context, snapshot) {
+                  if(snapshot.hasData){
+                    var liste;
+                    for (liste in snapshot.data!){
+                      checkIt.add(liste);
                     }
-                    if (snapshot.hasError) {
-                    // Gérez les erreurs ici
-                    return Text("Error: ${snapshot.error}");
-                    }
-                    // Utilisez la valeur une fois qu'elle est chargée
-                    bool? state = snapshot.data;
-                    return Checkbox(
-                    value: state,
-                    onChanged: (value) {
-                    setState(() {
-                    checkIt[index]['checked'] = value;
-                    TaskController.setTaskState(checkIt[index]['text'], value!);
-                    });
-                    TaskController.getTaskState(checkIt[index]['text']).then((bool?state){
-                      print("${checkIt[index]['text']} is $state");
-                    });
-                    },
-                    );
-                    },
-                    ),
+                    return ListView.builder(
+                    itemCount: checkIt.length,
+                    itemBuilder: (context, index) {
+                      return Row(
+                        children: [
+                      FutureBuilder<bool?>(
+                      future: TaskController.getTaskState(snapshot.data![index]['task_name'].toString()), // Obtenez l'état
+                      builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                      // Affichez un indicateur de chargement en attendant les données
+                      return const CircularProgressIndicator();
+                      }
+                      if (snapshot.hasError) {
+                      // Gérez les erreurs ici
+                      return Text("Error: ${snapshot.error}");
+                      }
+                      // Utilisez la valeur une fois qu'elle est chargée
+                      bool? state = snapshot.data;
+                      return Checkbox(
+                      value: state,
+                      onChanged: (value) {
+                      setState(() {
+                      checkIt[index]['checked'] = value;
+                      print(checkIt[index]['checked']);
+                      TaskController.setTaskState(checkIt[index]['task_name'], value!);
+                      });
+                      },
+                      );
+                      },
+                      ),
 
-                    Expanded(
-                          child: Text(checkIt[index]['text']),
-                        ),
-                      ],
-                    );
-                  })),
+                      Expanded(
+                            child: Text(checkIt[index]['task_name']??""),
+                          ),
+                        ],
+                      );
+                    });}
+                  else if (snapshot.hasError){
+                    const Text("Error");
+                  }
+                  else if (snapshot.connectionState == ConnectionState.waiting){return CircularProgressIndicator();}
+                  else {return Text("${snapshot.data ?? ''}");}
+                  return const Text("");
+                },
+              )),
         ],
       )
 
